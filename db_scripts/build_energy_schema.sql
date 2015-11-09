aa24d69e7cbad4dab8b7439f6dd39943cc6779cd
Philip O. Otenyo <philip.otenyo@pentaglobal.co.ke>
2015-11-09 17:03:17 +0200

    Energy Schema Build Scripts created

---
+++ db_scripts/build_energy_schema.sql
@@ -0,0 +1,17 @@
+
+
+@@create_user
+
+REM If this fails with ORA-01940 because energy_adf user is connected, issue the following as system:
+REM select sid, serial#, username from v$session where username='energy_adf';
+REM alter system kill session '<SID>,<SERIAL#>' immediate;
+
+REM Modify the next line to point to your database SID if not local (connect energy_adf/energy_adf@SID)
+
+connect energy_adf/energy_adf
+@@create_tables
+@@create_sequences
+@@create_functions
+@@create_packages
+@@create_triggers
+commit;
